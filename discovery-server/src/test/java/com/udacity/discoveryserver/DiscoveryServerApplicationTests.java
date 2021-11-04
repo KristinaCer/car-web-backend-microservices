6afd5e26835ff72437ec7165923ed6f39b05f93b
Kristina Cerniauskaite <k.cerniauskaite@gmail.com>
2021-11-04 11:53:58 +0100

    Added eureka discovery service and registered clients

--- discovery-server/src/test/java/com/udacity/discoveryserver/DiscoveryServerApplicationTests.java
+++ discovery-server/src/test/java/com/udacity/discoveryserver/DiscoveryServerApplicationTests.java
@@ -1,13 +1,13 @@
-package com.udacity.eurekaserver;
+package com.udacity.discoveryserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EurekaServerApplicationTests {
+class DiscoveryServerApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
